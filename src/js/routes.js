160107ae5c5e959d6ee367d7591a300a2ea50631
core2drew <calupesandro@gmail.com>
2017-09-09 03:50:34 +0800

    refactoring routes and adding new route

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch, browserHistory} from 'react-router-dom';
 import AfcaApp from './containers/AfcaApp';
-import Gallery from './containers/Gallery';
-import Home from './containers/Home';
+import {Home, AboutUs, Gallery} from './containers/pages'
 
 const Routes = (
     <Router history={browserHistory}>
         <AfcaApp>
             <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/aboutus" component={AboutUs}/>
                 <Route exact path="/gallery" component={Gallery}/>
             </Switch>
         </AfcaApp>
