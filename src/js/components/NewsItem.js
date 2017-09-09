1f3ff20ce3b5607e2e60530acbfcc3eae4eb0ded
core2drew <calupesandro@gmail.com>
2017-09-09 23:32:06 +0800

    Update NewsItem

--- src/js/components/NewsItem.js
+++ src/js/components/NewsItem.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import 'less/newsitem.less'
 
 const NewsItem = (props) => (
-    <div className="news-item uk-flex">
+    <div className="news-item uk-flex uk-position-relative">
+        <div className="readme uk-width-1-1 uk-height-1-1 uk-position-absolute uk-flex uk-flex-center uk-flex-middle">
+            <h5 className="uk-header uk-margin-remove">Read More</h5>
+        </div>
         <div className="image uk-background-cover" style={{
             'backgroundImage':`url(http://www.laughspark.info/uploadfiles/funny-sqiuurel-talking-imags-i-5922.jpg)`
         }}></div>
         <div className="description uk-margin-small-left">
             <h5 className="uk-header date uk-text-bold">09/09/17</h5>
             <dt>Title</dt>
-            <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
+            <dd className="uk-text-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
         </div>
     </div>
 )
