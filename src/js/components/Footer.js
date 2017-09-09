979cda69e53280e6976c3e3ea2e4968bd5212292
core2drew <calupesandro@gmail.com>
2017-09-09 23:31:42 +0800

    Update Footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -3,7 +3,7 @@ import 'less/footer.less';
 
 const Footer = (props) => (
     <div id='Footer' className='uk-position-absolute uk-width-1-1 uk-flex uk-flex-center uk-flex-middle'>
-        <p className="uk-text-small uk-margin-remove">Abraham's Flock Christian Academy 2018</p>
+        <p className="uk-text-small uk-margin-remove">Abraham's Flock Christian Academy 2017</p>
     </div>
 );
 
