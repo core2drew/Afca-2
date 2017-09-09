be55e958eaf45c6de2f9855ba03564c0ab042281
core2drew <calupesandro@gmail.com>
2017-09-09 23:31:57 +0800

    Update Slider

--- src/js/components/Slider.js
+++ src/js/components/Slider.js
@@ -61,7 +61,7 @@ class Slider extends Component {
                             Ang selebrasyon ng Buwan ng Wika ay noong Agosto 26, 2016. Ito ay ipinagdiriwang upang bigyang halaga ang ating sariling wika. Nagkaroon kami ng iba't ibang gawain sa bawat baitang. Para sa mga Preschool may coloring contest, sa mga Primary naman ay nagpaligsahan sila sa pagbigkas ng tula. Sa mga Intermediate naman ay ang paggawa ng poster at ang huli ay ang High school na gumawa ng tula tungkol sa tema na 'Filipino:Wika ng Karunungan'. Nagkaroon din kami ng food stalls na nagbebenta ng mga pagkaing Pilipino at ukay-ukay.
                         </span>
                     </div>
-                    <button className="uk-button readme">Read More</button>
+                    <button className="uk-button readme uk-text-capitalize">Read More</button>
                 </div>
             </div>
         )
